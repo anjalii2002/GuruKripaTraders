d09466688cce1a207c3a87aec25f19c5d4d4e541
Gurukripa Dev <dev@gurukripatraders.com>
2026-08-12 15:42:54 +0530

    Revert "Elevate landing page experience with HeroTrustStrip and PoojaOilComparison interactive section"
    This reverts commit 4fdcc02d3e7a9b1a045c94ed983a902472944570.

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,23 @@
 import Hero from '@/components/Hero';
-import HeroTrustStrip from '@/components/HeroTrustStrip';
 import CategoryShowcase from '@/components/CategoryShowcase';
 import ProductCatalog from '@/components/ProductCatalog';
-import PoojaOilComparison from '@/components/PoojaOilComparison';
 import BundleBuilder from '@/components/BundleBuilder';
+import VedicRitualGuide from '@/components/VedicRitualGuide';
 import PurityGuarantee from '@/components/PurityGuarantee';
 import Testimonials from '@/components/Testimonials';
 import FaqAccordion from '@/components/FaqAccordion';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#FAF6F0]">
+    <>
       <Hero />
-      <HeroTrustStrip />
-      <CategoryShowcase />
+      {/* <CategoryShowcase /> */}
       <ProductCatalog />
-      <PoojaOilComparison />
       <BundleBuilder />
-      <PurityGuarantee />
+      {/* <VedicRitualGuide /> */}
+      {/* <PurityGuarantee /> */}
       <Testimonials />
       <FaqAccordion />
-    </main>
+    </>
   );
 }
